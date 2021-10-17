3b494d464990743da38d42ea28bef6dc225f3ce5
Zein Fahrozi <m.zein@majoo.id>
2021-10-17 11:02:21 +0700

    Add middleware for JWT

---
+++ main-svc/middleware/jwt.go
@@ -0,0 +1,48 @@
+package middleware
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type JwtCustomClaims struct {
+	ID int `json:"id"`
+	jwt.StandardClaims
+}
+
+type ConfigJWT struct {
+	SecretJWT       string
+	ExpiresDuration int64
+}
+
+func (jwtConf *ConfigJWT) Init() middleware.JWTConfig {
+	return middleware.JWTConfig{
+		Claims:     &JwtCustomClaims{},
+		SigningKey: []byte(jwtConf.SecretJWT),
+		ErrorHandlerWithContext: middleware.JWTErrorHandlerWithContext(func(e error, c echo.Context) error {
+			return c.JSON(http.StatusUnauthorized, "User is unauthorized")
+		}),
+	}
+}
+
+func (jwtConf *ConfigJWT) GenerateToken(userID int) string {
+	claims := JwtCustomClaims{
+		userID,
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Hour * time.Duration(jwtConf.ExpiresDuration)).Unix(),
+		},
+	}
+	initToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	token, _ := initToken.SignedString([]byte(jwtConf.SecretJWT))
+	return token
+}
+
+func GetUser(c echo.Context) *JwtCustomClaims {
+	user := c.Get("user").(*jwt.Token)
+	claims := user.Claims.(*JwtCustomClaims)
+	return claims
+}
